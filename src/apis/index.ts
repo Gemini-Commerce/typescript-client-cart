1812c8b812390ddef40439a764a7fa9bbb326fab
Simone Marcato <simone@hevelop.com>
2024-06-04 14:41:40 +0200

    Generated client for service: cart with version: 1.0.0 in language: typescript

---
+++ src/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AddressesApi';
+export * from './BasicOperationsApi';
+export * from './CartApi';
+export * from './PaymentsApi';
+export * from './PromotionsApi';
+export * from './ShipmentsApi';
